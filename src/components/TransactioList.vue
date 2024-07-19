8c7086e0c361e304d19ace9af08de5e596dee8ba
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-07-19 20:54:45 +0300

    data binding added. toastification added

--- src/components/TransactioList.vue
+++ src/components/TransactioList.vue
@@ -1,6 +1,75 @@
+<!-- <script>
+// Options API way
+// export default {
+//   data() {
+//     return {
+//       transactions: [
+//         { id: 1, text: 'Flower', amount: -19.99 },
+//         { id: 2, text: 'Salary', amount: 299.97 },
+//         { id: 3, text: 'Book', amount: -10 },
+//         { id: 4, text: 'Camera', amount: 150 }
+//       ]
+//     }
+//   }
+// }
+
+// Composition api
+export default {
+  setup() {
+    const transactions = [
+      { id: 1, text: 'Flower', amount: -19.99 },
+      { id: 2, text: 'Salary', amount: 299.97 },
+      { id: 3, text: 'Book', amount: -10 },
+      { id: 4, text: 'Camera', amount: 150 }
+    ]
+    return {
+      transactions
+    }
+  }
+}
+</script> -->
+
+<!-- Newer composition api -->
+<script setup>
+// const transactions = [
+//   { id: 1, text: 'Flower', amount: -19.99 },
+//   { id: 2, text: 'Salary', amount: 299.97 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ]
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
+    <ul id="list" class="list">
+      <!-- For looping we are using the v-for directive -->
+      <!-- <li
+        v-for="transaction in transactions"
+        v-bind:key="transaction.id"
+        v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'"
+      >
+        {{ transaction.text }} <span>{{ transaction.amount }}</span
+        ><button class="delete-btn">x</button>
+      </li> -->
+      <!-- or: -->
+      <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"
+      >
+        {{ transaction.text }} <span>${{ transaction.amount }}</span
+        ><button class="delete-btn">x</button>
+      </li>
+    </ul>
     <!-- <li class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
     <li class="plus">Paycheck <span>$780</span><button class="delete-btn">x</button></li> -->
   </ul>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
-<script>
+<!-- Options api -->
+<!-- <script>
 import Header from '../components/Header.vue'
 import Balance from '../components/Balance.vue'
 import IncomeExpenses from '@/components/IncomeExpenses.vue'
@@ -15,24 +16,66 @@ export default {
     AddTransaction
   }
 }
-</script>
+</script> -->
 
-<!-- <script setup>
+<!-- new composition api -->
+<script setup>
 import Header from '../components/Header.vue'
 import Balance from '../components/Balance.vue'
 import IncomeExpenses from '@/components/IncomeExpenses.vue'
 import TransactioList from '@/components/TransactioList.vue'
 import AddTransaction from '@/components/AddTransaction.vue'
-</script> -->
+
+import { ref, computed } from 'vue'
+
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+])
+
+// console.log(transactions.value)
+
+// Get Total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0)
+})
+
+// Get Income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+    .toFixed(2) // toFixed adds decimals
+})
+
+// Get Expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+    .toFixed(2) // toFixed adds decimals
+})
+</script>
 
 <template>
   <main>
     <!-- <div class="mainPage"> -->
     <Header />
     <div class="container">
-      <Balance />
-      <IncomeExpenses />
-      <TransactioList />
+      <!-- v-bind can be removed -->
+      <Balance v-bind:total="total" />
+      <!-- + sign to pass props as numbers-->
+      <IncomeExpenses :income="+income" :expenses="+expenses" />
+      <!-- passing transactions as a prop to the component -->
+      <TransactioList :transactions="transactions" />
       <AddTransaction />
     </div>
     <!-- </div> -->
